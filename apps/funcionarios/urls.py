8286af677f5877c46e5cc91d3e9cb28b51cd10d9
Natalia <nataliamatricardi@gmail.com>
2025-11-12 20:55:41 -0300

    fix/urls.py de funcionarios

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,15 +1,7 @@
-
 from rest_framework.routers import DefaultRouter
-from .views import FuncionarioViewSet, perfil_view, alterar_senha_view
-from django.urls import path
+from .views import FuncionarioViewSet
 
 router = DefaultRouter()
 router.register(r'funcionarios', FuncionarioViewSet, basename="funcionario")
 
-urlpatterns = [
-    path('perfil/', perfil_view, name='perfil'),
-    path('alterar-senha/', alterar_senha_view, name='alterar_senha'),
-]
-
-
-urlpatterns += router.urls
+urlpatterns = router.urls
