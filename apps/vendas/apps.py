04d7fc7293815fa5698d95c675670da87dcc312d
mauricio <mauriciossf12@hotmail.com>
2025-11-25 18:20:17 -0300

    remove tudo relacionado a vendas

--- apps/vendas/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class VendasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.vendas'  # <--- Use o caminho completo aqui!
-    label = 'vendas'     # <--- Use o nome curto aqui!
-    verbose_name = 'Módulo de Vendas'
